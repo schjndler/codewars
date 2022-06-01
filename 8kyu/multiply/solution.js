5d8ce5475de9035399edcfb3f44f66cd90c5af5e
Tom Schindler <92883410+schjndler@users.noreply.github.com>
2022-06-01 22:27:55 +0200

    added first codewars

---
+++ 8kyu/multiply/solution.js
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//      a * b
+//   }
+
+// My Solution
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply);
