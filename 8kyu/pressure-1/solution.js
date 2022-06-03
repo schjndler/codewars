01884ea5762b1fa436d51e9ee2176882c8fa944b
Tom Schindler <92883410+schjndler@users.noreply.github.com>
2022-06-03 19:00:34 +0200

    kata finished

---
+++ 8kyu/pressure-1/solution.js
@@ -0,0 +1,12 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+// function doubleInteger(i) {
+//     // i will be an integer. Double it and return it.
+//     return i;
+//   }
+
+//   My Solution
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
